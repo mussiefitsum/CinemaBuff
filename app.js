25332d257cb6a5f4b0cfbbfed003e0c5ec79ac61
Mussie <mussie.fitsum@gmail.com>
2021-08-23 19:04:46 -0400

    Basic ejs templates created

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const ejsMate = require('ejs-mate')
 const path = require('path')
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+
+const key = process.env.TMDB_API_KEY;
 
 const config = async () => {
     const tool = await axios.get('https://api.themoviedb.org/3/configuration?api_key=85f4e309aef16b676f58113671a92502');
@@ -18,10 +26,16 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/movies', async (req, res) => {
-    const discover = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=85f4e309aef16b676f58113671a92502&language=en-US&region=US&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate');
+    const discover = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${ key }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
     const movies = discover.data.results;
-    res.render('template', { movies })
+    res.render('movies/index', { movies });
 });
+
+app.get('/tv', async (req, res) => {
+    const discover = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${ key }&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate`);
+    const shows = discover.data.results;
+    res.render('tv/index', { shows });
+})
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
