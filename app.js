c2149900e465be9aa09c6d435529f09df86c8d7e
Mussie <mussie.fitsum@gmail.com>
2021-09-11 18:08:40 -0400

    finished details template for TV Shows

--- app.js
+++ app.js
@@ -46,19 +46,46 @@ app.get('/movies/:id', async (req, res) => {
     const movieCredits = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/credits?api_key=${ key }&language=en-US`);
     const movieAgeRating = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/release_dates?api_key=${ key }`);
     const movieVideos = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/videos?api_key=${ key }&language=en-US`);
-    const similarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/similar?api_key=${ key }&language=en-US&page=1`)
+    const similarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/similar?api_key=${ key }&language=en-US&page=1`);
+    const recommendedMovies = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/recommendations?api_key=${ key }&language=en-US&page=1`);
     const cast = movieCredits.data.cast;
     const crew = movieCredits.data.crew;
     const movie = movieDetails.data;
     const similarTitles = similarMovies.data.results;
+    const recommendations = recommendedMovies.data.results;
+    const videos = movieVideos.data.results;
     const ageRating = movieAgeRating.data.results.filter(x => x.iso_3166_1 === 'US').map(x => x.release_dates).flat();
     const genres = movieDetails.data.genres.map(x => x.name);
     const directors = crew.filter(x => x.job === 'Director').map(x => x.name);
     const writers = crew.filter(x => x.job === 'Writer').map(x => x.name);
     const movieRuntime = (movie.runtime / 60).toString();
-    const videos = movieVideos.data.results;
-    res.render('movies/details', { movie, cast, crew, directors, writers, genres, ageRating, movieRuntime, videos, similarTitles });
+    res.render('movies/details', { movie, cast, crew, directors, writers, genres, ageRating, movieRuntime, videos, similarTitles, recommendations });
 });
+
+app.get('/tv/:id', async (req, res) => {
+    const { id } = req.params;
+    const tvDetails = await axios.get(`https://api.themoviedb.org/3/tv/${ id }?api_key=${ key }&language=en-US`);
+    const tvAgeRating = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/content_ratings?api_key=${ key }&language=en-US`);
+    const credits = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/credits?api_key=${ key }&language=en-US`);
+    const tvVideos = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/videos?api_key=${ key }&language=en-US`);
+    const similarShows = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/similar?api_key=${ key }&language=en-US&page=1`);
+    const recommendedShows = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/recommendations?api_key=${ key }&language=en-US&page=1`);
+    const show = tvDetails.data;
+    const cast = credits.data.cast;
+    const videos = tvVideos.data.results
+    const ratings = tvAgeRating.data.results
+    const similars = similarShows.data.results;
+    const recommendations = recommendedShows.data.results;
+    let rating = ratings.find(x => x.iso_3166_1 === 'US');
+    if (rating === undefined) {
+        rating = ratings.pop().rating;
+    } else {
+        rating = rating.rating;
+    }
+    const genres = tvDetails.data.genres.map(x => x.name).join(', ');
+    const runTime = Math.round(show.episode_run_time.reduce((a, b) => a + b) / show.episode_run_time.length);
+    res.render('tv/details', { show, rating, genres, cast, videos, runTime, similars, recommendations })
+})
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
