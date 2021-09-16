4a803b70f3c81833943cdd561d768480ed2f6ecc
Mussie <mussie.fitsum@gmail.com>
2021-09-15 20:53:44 -0400

    added authentication

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const axios = require('axios');
 const ejsMate = require('ejs-mate');
 const path = require('path');
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
+const catchAsync = require('./utility/catchAsync');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
 
 const User = require('./models/users')
 
@@ -25,6 +29,30 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const secret = 'topsecret15'
+
+const store = MongoStore.create({
+    mongoUrl: 'mongodb://localhost:27017/CinemaBuff',
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+const sessionConfig = {
+    store,
+    name: 'sess1500',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
@@ -34,34 +62,50 @@ const key = process.env.TMDB_API_KEY;
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+const isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
+        req.flash('error', 'Need to be logged in!');
+        return res.redirect('/login');
+    }
+    next();
+}
 
 
 app.get('/', async (req, res) => {
     res.send('Home');
 });
 
-app.get('/movies', async (req, res) => {
+app.get('/movies', catchAsync(async (req, res) => {
     const discoverOne = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${ key }&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
     const discoverTwo = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${ key }&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_watch_monetization_types=flatrate`);
     const movies = discoverOne.data.results;
     const pageTwo = discoverTwo.data.results;
     movies.push(...pageTwo);
     res.render('movies/index', { movies });
-});
+}));
 
-app.get('/tv', async (req, res) => {
+app.get('/tv', catchAsync(async (req, res) => {
     const discoverOne = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${ key }&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&watch_region=US&with_watch_monetization_types=flatrate`);
     const discoverTwo = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${ key }&language=en-US&sort_by=popularity.desc&page=2&timezone=America%2FNew_York&include_null_first_air_dates=false&watch_region=US&with_watch_monetization_types=flatrate`)
     const shows = discoverOne.data.results;
     const pageTwo = discoverTwo.data.results;
     shows.push(...pageTwo);
     res.render('tv/index', { shows });
-});
+}));
 
-app.get('/movies/:id', async (req, res) => {
+app.get('/movies/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const movieDetails = await axios.get(`https://api.themoviedb.org/3/movie/${ id }?api_key=${ key }&language=en-US`);
     const movieCredits = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/credits?api_key=${ key }&language=en-US`);
@@ -81,9 +125,9 @@ app.get('/movies/:id', async (req, res) => {
     const writers = crew.filter(x => x.job === 'Writer').map(x => x.name);
     const movieRuntime = (movie.runtime / 60).toString();
     res.render('movies/details', { movie, cast, crew, directors, writers, genres, ageRating, movieRuntime, videos, similarTitles, recommendations });
-});
+}));
 
-app.get('/tv/:id', async (req, res) => {
+app.get('/tv/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const tvDetails = await axios.get(`https://api.themoviedb.org/3/tv/${ id }?api_key=${ key }&language=en-US`);
     const tvAgeRating = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/content_ratings?api_key=${ key }&language=en-US`);
@@ -106,11 +150,45 @@ app.get('/tv/:id', async (req, res) => {
     const genres = tvDetails.data.genres.map(x => x.name).join(', ');
     const runTime = Math.round(show.episode_run_time.reduce((a, b) => a + b) / show.episode_run_time.length);
     res.render('tv/details', { show, rating, genres, cast, videos, runTime, similars, recommendations })
-})
+}))
 
-app.get('/register', async (req, res) => {
+app.get('/register', (req, res) => {
     res.render('users/register');
 })
+
+app.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to CinemaBuff!');
+            res.redirect('/movies');
+        });
+    }
+    catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}));
+
+app.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+app.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), catchAsync(async (req, res) => {
+    req.flash('success', 'Welcome Back!')
+    const redirectUrl = req.session.returnTo || '/movies';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}));
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Successfully logged out!');
+    res.redirect('/movies');
+});
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
