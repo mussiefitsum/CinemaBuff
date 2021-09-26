2e583bc7f00e96828b1e27d2f969162521a4868e
Mussie <mussie.fitsum@gmail.com>
2021-09-26 19:56:14 -0400

    slight router and design changes

--- app.js
+++ app.js
@@ -117,52 +117,64 @@ app.get('/results', async (req, res) => {
 });
 
 app.get('/movie/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const movieDetails = await axios.get(`https://api.themoviedb.org/3/movie/${ id }?api_key=${ key }&language=en-US`);
-    const movieCredits = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/credits?api_key=${ key }&language=en-US`);
-    const movieAgeRating = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/release_dates?api_key=${ key }`);
-    const movieVideos = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/videos?api_key=${ key }&language=en-US`);
-    const similarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/similar?api_key=${ key }&language=en-US&page=1`);
-    const recommendedMovies = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/recommendations?api_key=${ key }&language=en-US&page=1`);
-    const cast = movieCredits.data.cast;
-    const crew = movieCredits.data.crew;
-    const movie = movieDetails.data;
-    const similarTitles = similarMovies.data.results;
-    const recommendations = recommendedMovies.data.results;
-    const videos = movieVideos.data.results;
-    const ageRating = movieAgeRating.data.results.filter(x => x.iso_3166_1 === 'US').map(x => x.release_dates).flat();
-    const genres = movieDetails.data.genres.map(x => x.name);
-    const directors = crew.filter(x => x.job === 'Director').map(x => x.name);
-    const writers = crew.filter(x => x.job === 'Writer').map(x => x.name);
-    const movieRuntime = (movie.runtime / 60).toString();
-    const reviews = await Review.find({ contentId: id }).populate('author');
-    res.render('movies/details', { movie, cast, crew, directors, writers, genres, ageRating, movieRuntime, videos, similarTitles, recommendations, reviews });
+    try {
+        const { id } = req.params;
+        const movieDetails = await axios.get(`https://api.themoviedb.org/3/movie/${ id }?api_key=${ key }&language=en-US`);
+        const movieCredits = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/credits?api_key=${ key }&language=en-US`);
+        const movieAgeRating = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/release_dates?api_key=${ key }`);
+        const movieVideos = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/videos?api_key=${ key }&language=en-US`);
+        const similarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/similar?api_key=${ key }&language=en-US&page=1`);
+        const recommendedMovies = await axios.get(`https://api.themoviedb.org/3/movie/${ id }/recommendations?api_key=${ key }&language=en-US&page=1`);
+        const cast = movieCredits.data.cast;
+        const crew = movieCredits.data.crew;
+        const movie = movieDetails.data;
+        const similarTitles = similarMovies.data.results;
+        const recommendations = recommendedMovies.data.results;
+        const videos = movieVideos.data.results;
+        const ageRating = movieAgeRating.data.results.filter(x => x.iso_3166_1 === 'US').map(x => x.release_dates).flat();
+        const genres = movieDetails.data.genres.map(x => x.name);
+        const directors = crew.filter(x => x.job === 'Director').map(x => x.name);
+        const writers = crew.filter(x => x.job === 'Writer').map(x => x.name);
+        const movieRuntime = (movie.runtime / 60).toString();
+        const reviews = await Review.find({ contentId: id }).populate('author');
+        res.render('movies/details', { movie, cast, crew, directors, writers, genres, ageRating, movieRuntime, videos, similarTitles, recommendations, reviews });
+    } catch (e) {
+        console.log(e);
+        req.flash('error', 'Oops something went wrong!');
+        res.redirect('/movie');
+    }
 }));
 
 app.get('/tv/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const tvDetails = await axios.get(`https://api.themoviedb.org/3/tv/${ id }?api_key=${ key }&language=en-US`);
-    const tvAgeRating = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/content_ratings?api_key=${ key }&language=en-US`);
-    const credits = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/credits?api_key=${ key }&language=en-US`);
-    const tvVideos = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/videos?api_key=${ key }&language=en-US`);
-    const similarShows = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/similar?api_key=${ key }&language=en-US&page=1`);
-    const recommendedShows = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/recommendations?api_key=${ key }&language=en-US&page=1`);
-    const show = tvDetails.data;
-    const cast = credits.data.cast;
-    const videos = tvVideos.data.results
-    const ratings = tvAgeRating.data.results
-    const similars = similarShows.data.results;
-    const recommendations = recommendedShows.data.results;
-    let rating = ratings.find(x => x.iso_3166_1 === 'US');
-    if (rating === undefined) {
-        rating = ratings.pop().rating;
-    } else {
-        rating = rating.rating;
+    try {
+        const { id } = req.params;
+        const tvDetails = await axios.get(`https://api.themoviedb.org/3/tv/${ id }?api_key=${ key }&language=en-US`);
+        const tvAgeRating = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/content_ratings?api_key=${ key }&language=en-US`);
+        const credits = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/credits?api_key=${ key }&language=en-US`);
+        const tvVideos = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/videos?api_key=${ key }&language=en-US`);
+        const similarShows = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/similar?api_key=${ key }&language=en-US&page=1`);
+        const recommendedShows = await axios.get(`https://api.themoviedb.org/3/tv/${ id }/recommendations?api_key=${ key }&language=en-US&page=1`);
+        const show = tvDetails.data;
+        const cast = credits.data.cast;
+        const videos = tvVideos.data.results
+        const ratings = tvAgeRating.data.results
+        const similars = similarShows.data.results;
+        const recommendations = recommendedShows.data.results;
+        let rating = ratings.find(x => x.iso_3166_1 === 'US');
+        if (rating === undefined) {
+            rating = ratings.pop().rating;
+        } else {
+            rating = rating.rating;
+        }
+        const genres = tvDetails.data.genres.map(x => x.name).join(', ');
+        const runTime = Math.round(show.episode_run_time.reduce((a, b) => a + b) / show.episode_run_time.length);
+        const reviews = await Review.find({ contentId: id }).populate('author');
+        res.render('tv/details', { show, rating, genres, cast, videos, runTime, similars, recommendations, reviews })
+    } catch (e) {
+        console.log(e);
+        req.flash('error', 'Oops something went wrong!');
+        res.redirect('/tv');
     }
-    const genres = tvDetails.data.genres.map(x => x.name).join(', ');
-    const runTime = Math.round(show.episode_run_time.reduce((a, b) => a + b) / show.episode_run_time.length);
-    const reviews = await Review.find({ contentId: id }).populate('author');
-    res.render('tv/details', { show, rating, genres, cast, videos, runTime, similars, recommendations, reviews })
 }))
 
 app.post('/movie/:id/reviews', catchAsync(async (req, res) => {
@@ -173,14 +185,14 @@ app.post('/movie/:id/reviews', catchAsync(async (req, res) => {
     review.contentId = id;
     await review.save();
     req.flash('success', 'Successfully posted review!');
-    res.redirect(`/movies/${ id }`);
+    res.redirect(`/movie/${ id }`);
 }));
 
 app.delete('/movie/:id/reviews/:reviewId', catchAsync(async (req, res) => {
     const { reviewId, id } = req.params;
     await Review.findByIdAndDelete(reviewId);
     req.flash('success', 'Successfully deleted review!');
-    res.redirect(`/movies/${ id }`);
+    res.redirect(`/movie/${ id }`);
 }));
 
 app.post('/tv/:id/reviews', catchAsync(async (req, res) => {
@@ -213,7 +225,7 @@ app.post('/register', catchAsync(async (req, res) => {
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success', 'Welcome to CinemaBuff!');
-            res.redirect('/movies');
+            res.redirect('/movie');
         });
     }
     catch (e) {
@@ -228,7 +240,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), catchAsync(async (req, res) => {
     req.flash('success', 'Welcome Back!')
-    const redirectUrl = req.session.returnTo || '/movies';
+    const redirectUrl = req.session.returnTo || '/movie';
     delete req.session.returnTo;
     res.redirect(redirectUrl);
 }));
@@ -236,7 +248,7 @@ app.post('/login', passport.authenticate('local', { failureFlash: true, failureR
 app.get('/logout', (req, res) => {
     req.logout();
     req.flash('success', 'Successfully logged out!');
-    res.redirect('/movies');
+    res.redirect('/movie');
 });
 
 app.listen(3000, () => {
