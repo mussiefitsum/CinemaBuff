1f3cb1dd2bfa35c0f0530fd22a3019de27354d15
Mussie <mussie.fitsum@gmail.com>
2021-10-08 19:37:03 -0400

    added start script attempting to deploy

--- app.js
+++ app.js
@@ -109,6 +109,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('Listening on Port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on Port ${ port }`);
 });
